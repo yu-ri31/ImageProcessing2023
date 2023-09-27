c60dcda88383c3bbd3b2ef920880cff0c9935f23
OSAMU WATANABE <owatanab@es.takushoku-u.ac.jp>
2023-09-27 17:37:47 +0900

    Initial Commit

---
+++ test.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include <cstdlib>
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("Input image is required.\n");
+    return EXIT_FAILURE;
+  }
+
+  // 画像の読み込み
+  cv::Mat img = cv::imread(argv[1]);
+  if (img.empty()) {  // 画像ファイルが見つからない場合の処理
+    printf("Input image is not found.\n");
+    return EXIT_FAILURE;
+  }
+
+  // 画像の表示
+  cv::imshow("window", img);
+  // キー入力を待つ
+  cv::waitKey();
+  // 全てのウインドウを破棄
+  cv::destroyAllWindows();
+
+  return EXIT_SUCCESS;
+}
